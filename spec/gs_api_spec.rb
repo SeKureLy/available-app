34b03a449841c456322a1dc13c6078f717465239
RayhungKao <ryamkao2339@gmail.com>
2021-10-22 16:06:55 +0800

    happy test on parse result

--- spec/gs_api_spec.rb
+++ spec/gs_api_spec.rb
@@ -17,42 +17,46 @@ describe 'Tests Scopus API library' do
     @search_result = API.search('blockchain')
   end
   describe 'Check Raw Search Result' do
-    it 'HAPPY: should provide correct length' do
+    it '[raw]HAPPY: should provide correct length' do
       _(@search_result.size).must_equal RAW_CORRECT.size
     end
-    it 'HAPPY: title should be blockchain' do
+    it '[raw]HAPPY: title should be blockchain' do
       _(@search_result[0][:"dc:title"]).must_equal RAW_CORRECT[0][:"dc:title"]
     end
-    it 'HAPPY: eid should be same' do
+    it '[raw]HAPPY: eid should be same' do
       _(@search_result[0][:eid]).must_equal RAW_CORRECT[0][:eid]
     end
-    it 'HAPPY: cite-by-count should be same' do
+    it '[raw]HAPPY: cite-by-count should be same' do
       _(@search_result[0][:"citedby-count"]).must_equal RAW_CORRECT[0][:"citedby-count"]
     end
   end
 end
 
-# describe 'Check Parse Search Result' do
-#   before do
-#     @api_instance = Google::ScholarApi.new(API_TOKEN)
-#     @api_instance.search('Blockchain')
-#     @parse_result = @api_instance.parse
-#   end
-#   it 'HAPPY: should provide correct length' do
-#     _(@parse_result.size).must_equal PARSE_CORRECT.size
-#   end
-#   it 'HAPPY: title should be blockchain' do
-#     _(@parse_result[0][:title]).must_equal RAW_CORRECT[0][:title]
-#   end
-#   it 'HAPPY: position should be filtered' do
-#     _(assert_nil(@parse_result[0][:position]))
-#   end
-# end
+describe 'Check Parse Search Result' do
+  before do
+    # @api_instance = Google::ScholarApi.new(API_TOKEN)
+    # @api_instance.search('blockchain')
+    API.search('blockchain')
+    @parse_result = API.parse
+  end
+  it '[parse]HAPPY: should provide correct length' do
+    _(@parse_result.size).must_equal PARSE_CORRECT.size
+  end
+  it '[parse]HAPPY: title should be blockchain' do
+    _(@parse_result[0][:title]).must_equal PARSE_CORRECT[0][:title]
+  end
+  it '[parse]HAPPY: author should be blockchain' do
+    _(@parse_result[0][:author]).must_equal PARSE_CORRECT[0][:author]
+  end
+  it '[parse]HAPPY: prism:url should be parsed to link, so it is nil' do
+    _(assert_nil(@parse_result[0][:"prism:url"]))
+  end
+end
 
-# describe 'Error handling' do
-#   it 'should raise unauthorized' do
-#     _(proc do
-#         Google::ScholarApi.new('bad token').search('Blockchain')
-#       end).must_raise Google::ScholarApi::Errors::Unauthorized
-#   end
-# end
+describe 'Error handling' do
+  it 'should raise unauthorized' do
+    _(proc do
+        PaperDeep::ScopusAPI.new('bad token').search('Blockchain')
+      end).must_raise PaperDeep::ScopusAPI::Errors::Unauthorized
+  end
+end
