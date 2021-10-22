dbc999a35d48070a7dfe1707fcc44b1c8f08e55a
RayhungKao <ryamkao2339@gmail.com>
2021-10-22 16:53:04 +0800

    add vcr

--- spec/scopus_api_spec.rb
+++ spec/scopus_api_spec.rb
@@ -13,51 +13,72 @@ require_relative 'spec_helper'
 
 API = PaperDeep::ScopusAPI.new(API_TOKEN)
 
-describe 'Tests Scopus API library' do
-  before do
-    @search_result = API.search('blockchain')
-  end
-  describe 'Check Raw Search Result' do
-    it '[raw]HAPPY: should provide correct length' do
-      _(@search_result.size).must_equal RAW_CORRECT.size
-    end
-    it '[raw]HAPPY: title should be blockchain' do
-      _(@search_result[0][:"dc:title"]).must_equal RAW_CORRECT[0][:"dc:title"]
-    end
-    it '[raw]HAPPY: eid should be same' do
-      _(@search_result[0][:eid]).must_equal RAW_CORRECT[0][:eid]
-    end
-    it '[raw]HAPPY: cite-by-count should be same' do
-      _(@search_result[0][:"citedby-count"]).must_equal RAW_CORRECT[0][:"citedby-count"]
-    end
-  end
-end
+describe 'Tests Scopus API library with cassette' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
 
-describe 'Check Parse Search Result' do
-  before do
-    # @api_instance = Google::ScholarApi.new(API_TOKEN)
-    # @api_instance.search('blockchain')
-    API.search('blockchain')
-    @parse_result = API.parse
+    c.filter_sensitive_data('<SCOPUS_API_TOKEN>') { API_TOKEN }
+    c.filter_sensitive_data('<SCOPUS_API_TOKEN_ESC>') { CGI.escape(API_TOKEN) }
   end
-  it '[parse]HAPPY: should provide correct length' do
-    _(@parse_result.size).must_equal PARSE_CORRECT.size
-  end
-  it '[parse]HAPPY: title should be blockchain' do
-    _(@parse_result[0][:title]).must_equal PARSE_CORRECT[0][:title]
-  end
-  it '[parse]HAPPY: author should be blockchain' do
-    _(@parse_result[0][:author]).must_equal PARSE_CORRECT[0][:author]
-  end
-  it '[parse]HAPPY: prism:url should be parsed to link, so it is nil' do
-    _(assert_nil(@parse_result[0][:"prism:url"]))
-  end
-end
 
-describe 'Error handling' do
-  it 'should raise unauthorized' do
-    _(proc do
-        PaperDeep::ScopusAPI.new('bad token').search('Blockchain')
-      end).must_raise PaperDeep::ScopusAPI::Errors::Unauthorized
+  before do
+    VCR.insert_cassette CASSETTE_FILE,
+                        record: :new_episodes,
+                        match_requests_on: %i[method uri headers]
+  end
+
+  after do
+    VCR.eject_cassette
+  end
+
+  describe 'Tests Scopus API library' do
+
+    before do
+      @search_result = API.search('blockchain')
+    end
+    describe 'Check Raw Search Result' do
+      it '[raw]HAPPY: should provide correct length' do
+        _(@search_result.size).must_equal RAW_CORRECT.size
+      end
+      it '[raw]HAPPY: title should be blockchain' do
+        _(@search_result[0][:"dc:title"]).must_equal RAW_CORRECT[0][:"dc:title"]
+      end
+      it '[raw]HAPPY: eid should be same' do
+        _(@search_result[0][:eid]).must_equal RAW_CORRECT[0][:eid]
+      end
+      it '[raw]HAPPY: cite-by-count should be same' do
+        _(@search_result[0][:"citedby-count"]).must_equal RAW_CORRECT[0][:"citedby-count"]
+      end
+    end
+  end
+
+  describe 'Check Parse Search Result' do
+    before do
+      # @api_instance = Google::ScholarApi.new(API_TOKEN)
+      # @api_instance.search('blockchain')
+      API.search('blockchain')
+      @parse_result = API.parse
+    end
+    it '[parse]HAPPY: should provide correct length' do
+      _(@parse_result.size).must_equal PARSE_CORRECT.size
+    end
+    it '[parse]HAPPY: title should be blockchain' do
+      _(@parse_result[0][:title]).must_equal PARSE_CORRECT[0][:title]
+    end
+    it '[parse]HAPPY: author should be blockchain' do
+      _(@parse_result[0][:author]).must_equal PARSE_CORRECT[0][:author]
+    end
+    it '[parse]HAPPY: prism:url should be parsed to link, so it is nil' do
+      _(assert_nil(@parse_result[0][:"prism:url"]))
+    end
+  end
+
+  describe 'Error handling' do
+    it 'should raise unauthorized' do
+      _(proc do
+          PaperDeep::ScopusAPI.new('bad token').search('Blockchain')
+        end).must_raise PaperDeep::ScopusAPI::Errors::Unauthorized
+    end
   end
 end
