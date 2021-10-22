14035812c1dac8648b367042db35207a45bbe41f
WilsonCheng-sy <willie2524@gmail.com>
2021-10-22 14:42:37 +0800

    specify field in api

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -35,10 +35,10 @@ module Elsevier
 
     def parse
       organic_results.map do |origin_hash|
-        summary = origin_hash[:publication_info][:summary].split('-')
         {
-          title: origin_hash[:title],
-          link: origin_hash[:link],
+          eid: origin_hash[:eid],
+          title: origin_hash[:"dc:title"],
+          link: origin_hash[:"prism:url"],
           snippet: origin_hash[:snippet],
           journal: summary[1], author: summary[0],
           citeBy: origin_hash[:inline_links][:cited_by][:total]
@@ -50,14 +50,15 @@ module Elsevier
       config = YAML.safe_load(File.read('config/secrets.yml'))
       # api_key = config['api_key']
       # puts api_key
-      url = API_PROJECT_ROOT + "query=#{query}&sort=citedby-count&count=1&subj=COMP"
-      puts url
+      field = ["authname", "dc:title", "eid", "citedby-count", "prism:url", "prism:publicationName", "prism:coverDate", "affilname"].join(',')
+      url = API_PROJECT_ROOT + "query=#{query}&sort=citedby-count&count=1&filed=#{field}"
+      # puts url
       result = HTTP.headers('Accept' => 'application/json',
                             'X-ELS-APIKey' => "#{config['api_key']}").get(url)
       response_code = result.code
       raise(HTTP_ERROR[response_code]) if HTTP_ERROR.keys.include?(response_code)
 
-      @organic_results = JSON.parse(result, symbolize_names: true)
+      @organic_results = JSON.parse(result, symbolize_names: true)[:"search-results"][:entry]
       puts @organic_results
     end
   end
