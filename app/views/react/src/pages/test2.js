b96bd3e3308550bae542288a358ae8349ff73511
WilsonCheng-sy <willie2524@gmail.com>
2021-10-29 10:48:49 +0800

    citationTree

--- app/views/react/src/pages/test2.js
+++ app/views/react/src/pages/test2.js
@@ -1,25 +1,49 @@
 import React, { useState, useEffect } from "react";
+import { Tree, TreeNode } from 'react-organizational-chart';
+import styled, { css } from 'styled-components'
 
 import { Button, Navbar, Nav } from 'react-bootstrap'
 import logo from './../logo.svg';
 
 function Test2() {
+
+  const StyledNode = styled.div`
+  padding: 5px;
+  border-radius: 8px;
+  display: inline-block;
+  border: 1px solid red;
+`;
+
+  const StyledTreeExample = () => (
+    <Tree
+      lineWidth={'2px'}
+      lineColor={'green'}
+      lineBorderRadius={'10px'}
+      label={<StyledNode>Root</StyledNode>}
+    >
+      <TreeNode label={<StyledNode>Child 1</StyledNode>}>
+        <TreeNode label={<StyledNode>Grand Child</StyledNode>} />
+      </TreeNode>
+      <TreeNode label={<StyledNode>Child 2</StyledNode>}>
+        <TreeNode label={<StyledNode>Grand Child</StyledNode>}>
+          <TreeNode label={<StyledNode>Great Grand Child 1</StyledNode>} />
+          <TreeNode label={<StyledNode>Great Grand Child 2</StyledNode>} />
+        </TreeNode>
+      </TreeNode>
+      <TreeNode label={<StyledNode>Child 3</StyledNode>}>
+        <TreeNode label={<StyledNode>Grand Child 1</StyledNode>} />
+        <TreeNode label={<StyledNode>Grand Child 2</StyledNode>} />
+      </TreeNode>
+    </Tree>
+  );
+
   return (
     <>
       <div className="App">
-        <h1>Test2</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>CitationTree</h1>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <StyledTreeExample />
+
       </div>
     </>
   );
