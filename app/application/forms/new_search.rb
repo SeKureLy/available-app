f689ee0f9a2d06bf586523c6234b3530a72fd4d6
RayhungKao <ryamkao2339@gmail.com>
2021-11-25 15:35:31 +0800

    optimize paper search in app.rb by service object

--- app/application/forms/new_search.rb
+++ app/application/forms/new_search.rb
@@ -5,14 +5,14 @@ require 'dry-validation'
 module PaperDeep
   module Forms
     class NewSearch < Dry::Validation::Contract
-      URL_REGEX = /^[A-Za-z]+/.freeze
+      KEYWORD_REGEX = /^[A-Za-z]+/.freeze
 
       params do
         required(:keyword).filled(:string)
       end
 
       rule(:keyword) do
-        unless URL_REGEX.match?(value)
+        unless KEYWORD_REGEX.match?(value)
           key.failure('is an nonsense searching keyword')
         end
       end
