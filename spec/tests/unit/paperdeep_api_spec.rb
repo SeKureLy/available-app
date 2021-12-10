e8e5cf1dc8f2e83b5a050b13818fe6ff75af3563
RayhungKao <ryamkao2339@gmail.com>
2021-12-10 18:08:18 +0800

    update unit test of Gateway::Api

--- spec/tests/unit/paperdeep_api_spec.rb
+++ spec/tests/unit/paperdeep_api_spec.rb
@@ -1,24 +1,33 @@
 # frozen_string_literal: true
 
-require_relative '../../helpers/spec_helper.rb'
+require_relative '../../spec_helper.rb'
 
 describe 'Unit test of PaperDeep API gateway' do
   it 'must report alive status' do
-    alive = PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'}).alive?
+    alive = PaperDeep::Gateway::Api.new(PaperDeep::App.config).alive?
     _(alive).must_equal true
   end
 
-  it 'must be able to search for papers' do
-    res = PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
-      .search(KEYWORD)
-    
+  it 'must be able to search for papers with keyword' do
+    res = PaperDeep::Gateway::Api.new(PaperDeep::App.config)
+      .paper({'keyword': KEYWORD})
+
     _(res.success?).must_equal true
     _(res.payload).must_include KEYWORD
   end
 
+  it 'must be able to search for papers with EID' do
+    res = PaperDeep::Gateway::Api.new(PaperDeep::App.config)
+      .paper({'eid': EID})
+
+    puts res
+    _(res.success?).must_equal true
+    _(res.payload).must_include EID
+  end
+
   it 'must be able to search for publications' do
-    res = PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
-      .publication(PID)
+    res = PaperDeep::Gateway::Api.new(PaperDeep::App.config)
+      .publication({'pid': PID})
 
     _(res.success?).must_equal true
     _(res.payload).must_include PID
@@ -26,11 +35,11 @@ describe 'Unit test of PaperDeep API gateway' do
 
   it 'must be able to return all db papers' do
     # GIVEN a list of papers is added into the database
-    PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
-      .search(EID)
+    PaperDeep::Gateway::Api.new(PaperDeep::App.config)
+      .paper({'keyword': KEYWORD})
   
     # WHEN we request the papers
-    res = PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
+    res = PaperDeep::Gateway::Api.new(PaperDeep::App.config)
       .db_paper()
 
     # THEN we should see the very EID in the list of papers 
@@ -40,11 +49,11 @@ describe 'Unit test of PaperDeep API gateway' do
 
   it 'must be able to return db publications' do
     # GIVEN a publication is added into the database
-    PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
-      .publication(PID)
+    PaperDeep::Gateway::Api.new(PaperDeep::App.config)
+      .publication({'pid': PID})
 
     # WHEN we request the publications
-    res = PaperDeep::Gateway::Api.new({API_HOST: 'http://localhost:9292'})
+    res = PaperDeep::Gateway::Api.new(PaperDeep::App.config)
       .db_publication(EID)
 
     # THEN we should see the very PID in the list of publications
