4ab580557cdd88734227cb3959e52296dc716b13
RayhungKao <ryamkao2339@gmail.com>
2021-12-10 18:08:55 +0800

    update spec_helper

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -12,8 +12,6 @@ require_relative '../../init'
 
 ENV['RACK_ENV'] = 'test'
 
-# CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
-# API_TOKEN = CONFIG['test']['api_key']
 KEYWORD = 'blockchain'
 EID = '2-s2.0-84979828304'
 PID = '84979828304'
