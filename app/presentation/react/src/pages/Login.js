d119300e6047031bfab4caf6a4bd7faf3fe9562b
suvincent <suvincent0226@gmail.com>
2022-05-06 15:15:02 +0800

    react build

--- app/presentation/react/src/pages/Login.js
+++ app/presentation/react/src/pages/Login.js
@@ -1,30 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import queryString from 'query-string'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     useLocation,
-    Link
+    useHistory
 } from "react-router-dom";
 import { Button, Form, Col, InputGroup, Row, FormControl, Container, Table } from 'react-bootstrap'
 import { baseUrl } from '../config'
-
-
-
+import { AuthContext } from "../contexts";
 function Login(props) {
     const { search } = useLocation()
     const urlparams = queryString.parse(search)
+    const history = useHistory();
     const [init, setinit] = useState(false)
     const [account, setAccount] = useState("")
     const [password, setPassword] = useState("")
+    const { setUser } = useContext(AuthContext);
 
     useEffect(() => {
 
     }, []);
 
-    function login(){
+    async function login(event){
         // todo:  call login api
+        event.preventDefault()
+        const requestOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username: account, password:password })
+        };
+        fetch(baseUrl+'/api/v1/auth/login', requestOptions)
+        .then(async response =>{
+            let result = await response.json()
+            console.log(result)
+            props.alertSuccessFunction(`Welcome, ${result.account}`)
+            setUser(result.account)
+            setTimeout(()=>{
+                history.push('/')
+            },3000)
+        })
+        .catch(error =>{
+            console.log("login fail")
+            props.alertFunction("login fail")
+        })
     }
 
     return (
@@ -38,10 +60,10 @@ function Login(props) {
                 <Row>
                     <Col></Col>
                     <Col>
-                        <Form onSubmit={login}>
+                        <Form onSubmit={(e)=>{login(e)}}>
                             <Form.Group className="mb-3" controlId="formBasicEmail">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" value={account} onChange={(e)=>{setAccount(e.target.value)}} placeholder="Enter email" />
+                                <Form.Label>Account</Form.Label>
+                                <Form.Control type="text" value={account} onChange={(e)=>{setAccount(e.target.value)}} placeholder="Enter email" />
                                 <Form.Text className="text-muted">
                                     We'll never share your email with anyone else.
                                 </Form.Text>
