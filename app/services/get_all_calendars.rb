9635a629541b3076d355cc00d73ddd5b9255a77b
BryanHY <53143180+BryanHY@users.noreply.github.com>
2022-05-17 16:31:31 +0800

    Added "send Auth Token to API on calendar request"

---
+++ app/services/get_all_calendars.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'http'
+
+# Returns all projects belonging to an account
+class GetAllCalendars
+  def initialize(config)
+    @config = config
+  end
+
+  def call(current_account)
+    response = HTTP.auth("Bearer #{current_account.auth_token}")
+                   .get("#{@config.API_URL}/calendars")
+
+    response.code == 200 ? JSON.parse(response.to_s)['data'] : nil
+  end
+end
