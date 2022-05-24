4296eebffd1751a2292f33fceb478ea99af45e1a
RayhungKao <ryamkao2339@gmail.com>
2022-05-24 16:04:56 +0800

    add event model

---
+++ app/models/event.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'calendar'
+
+module Available    
+  # Behaviors of the currently logged in account
+  class Event
+    attr_reader :id, :title, :start_time, :end_time, :description, :created_by, :share_id, # basic info
+                :calendar # full details
+
+    def initialize(info)
+      process_attributes(info['attributes'])
+      process_included(info['include'])
+    end
+
+    private
+
+    def process_attributes(attributes)
+      @id               = attributes['id']
+      @title            = attributes['title']
+      @start_time       = attributes['start_time']
+      @end_time         = attributes['end_time']
+      @description      = attributes['description']
+      @created_by       = attributes['created_by']
+      @share_id         = attributes['share_id']
+    end
+
+    def process_included(included)
+      @calendar = Calendar.new(included['calendar'])
+    end
+  end
+end
