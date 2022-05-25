4780ad924ee67e3a28f7c025b625c1b58c23339f
RayhungKao <ryamkao2339@gmail.com>
2022-05-25 23:06:39 +0800

    update services

--- app/services/authenticate_account.rb
+++ app/services/authenticate_account.rb
@@ -16,9 +16,15 @@ module Available
       response = HTTP.post("#{@config.API_URL}/auth/authenticate",
                            json: { username:, password: })
 
-      raise(UnauthorizedError) unless response.code == 200
+      raise(UnauthorizedError) if response.code == 403
+      raise(ApiServerError) if response.code != 200
 
-      JSON.parse(response)
+      account_info = JSON.parse(response.to_s)['attributes']
+
+      { account: account_info['account']['data']['attributes'],
+        auth_token: account_info['auth_token'] }
+    rescue HTTP::ConnectionError
+      raise ApiServerError
     end
   end
 end
---
+++ app/services/get_event.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'http'
+
+module Available
+  # Returns an event belonging to a calendar
+  class GetEvent
+    def initialize(config)
+      @config = config
+    end
+
+    def call(user, event_id)
+      response = HTTP.auth("Bearer #{user.auth_token}")
+                    .get("#{@config.API_URL}/events/#{event_id}")
+
+      response.code == 200 ? JSON.parse(response.body.to_s)['data'] : nil
+    end
+  end
+end
