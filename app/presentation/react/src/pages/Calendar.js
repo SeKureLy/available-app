9558207c8fe9d6436ac89268cc2697df4b576bf9
suvincent <suvincent0226@gmail.com>
2022-06-07 22:45:33 +0800

    add events to front end

--- app/presentation/react/src/pages/Calendar.js
+++ app/presentation/react/src/pages/Calendar.js
@@ -19,11 +19,50 @@ function CalendarView(props) {
     const urlparams = queryString.parse(search)
     const [init, setinit] = useState(false)
     const [query, setQuery] = useState("")
-    const [myEventsList, setMyEventsList] = useState([
-        {title : "test", start : moment(), end : moment(), allDay : false},
-        {title : "test", start : moment(), end : moment(), allDay : true}
-    ])
+    const [myEventsList, setMyEventsList] = useState([])
     const { user, setUser } = useContext(AuthContext);
+
+    useEffect(() => {
+        if(user){
+            getCalendar()
+        }
+    }, []);
+
+
+    async function getCalendar(){
+        // todo:  call get calendars api
+        // const auth_token = 'Bearer ' + user.auth_token
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        };
+        fetch(baseUrl+'/api/v1/calendars/1', requestOptions)
+        .then(async response =>{
+            let result = await response.json()
+            console.log(result)
+
+            if (response.status === 200){
+                if(result.calendar){
+                    let events = result.calendar.events.map(e => ({
+                        title: e.title,
+                        start: new Date(e.start_time*1000),
+                        end: new Date(e.end_time*1000)
+                    }));
+                    setMyEventsList(events)
+                }
+            }
+            else{
+                props.alertFunction(`${result.message}`)
+            }
+        })
+        .catch(error =>{
+            console.log("+++++++++++++++++")
+            props.alertFunction(error.message)
+        })
+    }
 
     return (
         <>
@@ -38,10 +77,9 @@ function CalendarView(props) {
                 <Row>
                     <Col>
                         <Calendar
+                            defaultDate={new Date()}
                             localizer={localizer}
                             events={myEventsList}
-                            startAccessor="start"
-                            endAccessor="end"
                             style={{ height: 500 }}
                         />
                     </Col>
