3a672077632c9125180a0fa2a7294ffdd78f8402
suvincent <suvincent0226@gmail.com>
2022-06-14 19:23:33 +0800

    feature coordinate owned calendar

--- app/presentation/react/src/pages/Account.js
+++ app/presentation/react/src/pages/Account.js
@@ -18,20 +18,14 @@ function Account(props) {
     const [init, setinit] = useState(false)
     const [query, setQuery] = useState("")
     const { user, setUser } = useContext(AuthContext);
-    const [calendars, setCalendars] = useState(null)
+    const {calendars, setCalendars} = useContext(AuthContext)
     const { userInfo, setUserInfo } = useContext(AuthContext);
     const [CalendarInfo, setCalendarInfo] = useState(null)
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const [memberEmail, setMemberEmail] = useState("")
     const [calendarTitle, setCalendarTitle] = useState("")
-
-    useEffect(() => {
-        if (user) {
-            if (!userInfo) displayAccount()
-            if (!calendars) displayCalendars()
-        }
-    }, [user]);
+    const [guestLink, setGuestLink] = useState("")
 
     async function displayCalendars() {
         // todo:  call get calendars api
@@ -61,36 +55,11 @@ function Account(props) {
             })
     }
 
-    async function displayAccount() {
-        // todo:  call get calendars api
-        // const auth_token = 'Bearer ' + user.auth_token
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            credentials: 'include'
-        };
-        fetch(baseUrl + `/api/v1/account/${user}`, requestOptions)
-        .then(async response => {
-            let result = await response.json()
-            if (response.status === 200) {
-                setUserInfo(result)
-            }
-            else {
-                props.alertFunction(`${result.message}`)
-            }
-        })
-        .catch(error => {
-            props.alertFunction(error.message)
-        })
-    }
-
     function view(cid) {
         history.push(`/calendar?cid=${cid}`)
     }
 
-    function handleShow(cid){
+    function handleShow(cid) {
         const requestOptions = {
             method: 'GET',
             headers: {
@@ -108,87 +77,91 @@ function Account(props) {
             })
         setShow(true)
     }
-    function deleteMember(id){
+    function deleteMember(id) {
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email: CalendarInfo.calendar.members[id].email}),
+            body: JSON.stringify({ email: CalendarInfo.calendar.members[id].email }),
             credentials: 'include'
         };
-        fetch(baseUrl+`/api/v1/calendars/${CalendarInfo.calendar.id}/members?action=remove`, requestOptions)
-        .then(async response =>{
-            let result = await response.json()
-            if (response.status == 200){
-                props.alertSuccessFunction(`delete member successfully`)
-            }
-            else{
-                props.alertFunction(`${result.message}`)
-            }
-        })
-        .catch(error =>{
-          props.alertFunction("unknown error")
-        })
+        fetch(baseUrl + `/api/v1/calendars/${CalendarInfo.calendar.id}/members?action=remove`, requestOptions)
+            .then(async response => {
+                let result = await response.json()
+                if (response.status == 200) {
+                    props.alertSuccessFunction(`delete member successfully`)
+                }
+                else {
+                    props.alertFunction(`${result.message}`)
+                }
+            })
+            .catch(error => {
+                props.alertFunction("unknown error")
+            })
         setShow(false)
     }
 
-    function addMember(e){
+    function addMember(e) {
         e.preventDefault()
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ email: memberEmail}),
+            body: JSON.stringify({ email: memberEmail }),
             credentials: 'include'
         };
 
-        fetch(baseUrl+`/api/v1/calendars/${CalendarInfo.calendar.id}/members?action=add`, requestOptions)
-        .then(async response =>{
-            let result = await response.json()
-            if (response.status == 200){
-                props.alertSuccessFunction(`add member successfully`)
-            }
-            else{
-                props.alertFunction(`${result.message}`)
-            }
-        })
-        .catch(error =>{
-          props.alertFunction("unknown error")
-        })
+        fetch(baseUrl + `/api/v1/calendars/${CalendarInfo.calendar.id}/members?action=add`, requestOptions)
+            .then(async response => {
+                let result = await response.json()
+                if (response.status == 200) {
+                    props.alertSuccessFunction(`add member successfully`)
+                }
+                else {
+                    props.alertFunction(`${result.message}`)
+                }
+            })
+            .catch(error => {
+                props.alertFunction("unknown error")
+            })
         setShow(false)
         setMemberEmail("")
     }
 
-    function addCalendar(e){
+    function addCalendar(e) {
         e.preventDefault()
         const requestOptions = {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ title: calendarTitle}),
+            body: JSON.stringify({ title: calendarTitle }),
             credentials: 'include'
         };
 
-        fetch(baseUrl+`/api/v1/calendars`, requestOptions)
-        .then(async response =>{
-            let result = await response.json()
-            if (response.status == 200){
-                props.alertSuccessFunction(`add calendar successfully`)
-                setTimeout(() => {
-                    displayCalendars()
-                }, 3000)
-            }
-            else{
-                props.alertFunction(`${result.message}`)
-            }
-        })
-        .catch(error =>{
-          props.alertFunction("unknown error")
-        })
+        fetch(baseUrl + `/api/v1/calendars`, requestOptions)
+            .then(async response => {
+                let result = await response.json()
+                if (response.status == 200) {
+                    props.alertSuccessFunction(`add calendar successfully`)
+                    setTimeout(() => {
+                        displayCalendars()
+                    }, 3000)
+                }
+                else {
+                    props.alertFunction(`${result.message}`)
+                }
+            })
+            .catch(error => {
+                props.alertFunction("unknown error")
+            })
         setCalendarTitle("")
+    }
+
+    function addGuest(){
+        // setCalendarInfo
     }
 
     return (
@@ -204,99 +177,111 @@ function Account(props) {
                     <Modal.Title>Calendar Members</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                {
-                    (CalendarInfo && CalendarInfo.calendar) ?
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>account</th>
-                                    <th>email</th>
-                                    <th>type</th>
-                                    <th>actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td>{CalendarInfo.calendar.owner.username}</td>
-                                <td>{CalendarInfo.calendar.owner.email}</td>
-                                <td>owner</td>
-                                <td>
-                                </td>
-                                </tr>
-                                {CalendarInfo.calendar.members.map((member, id) => {
-                                    let c = member
-                                    return (
-                                        <tr key={id+1}>
-                                            <td>{c.username}</td>
-                                            <td>{c.email}</td>
-                                            <td>member</td>
-                                            <td>
-                                                <Button variant="danger" onClick={() => deleteMember(id)}>delete</Button>{' '}
-                                            </td>
-                                        </tr>)
-                                })}
-                            </tbody>
-                        </Table>
-                        : "empty calendars"
-                }
+                    {
+                        (CalendarInfo && CalendarInfo.calendar) ?
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>account</th>
+                                        <th>email</th>
+                                        <th>type</th>
+                                        <th>actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{CalendarInfo.calendar.owner.username}</td>
+                                        <td>{CalendarInfo.calendar.owner.email}</td>
+                                        <td>owner</td>
+                                        <td>
+                                        </td>
+                                    </tr>
+                                    {CalendarInfo.calendar.members.map((member, id) => {
+                                        let c = member
+                                        return (
+                                            <tr key={id + 1}>
+                                                <td>{c.username}</td>
+                                                <td>{c.email}</td>
+                                                <td>member</td>
+                                                <td>
+                                                    <Button variant="danger" onClick={() => deleteMember(id)}>delete</Button>{' '}
+                                                </td>
+                                            </tr>)
+                                    })}
+                                </tbody>
+                            </Table>
+                            : "empty calendars"
+                    }
                 </Modal.Body>
                 <Modal.Footer>
                     <Form.Group>
                         <Form.Control type="email" value={memberEmail} onChange={(e) => { setMemberEmail(e.target.value) }} placeholder="New Member Email" />
                     </Form.Group>
                     <Button variant="primary" onClick={(e) => addMember(e)}>Add member</Button>
-                    <Button variant="secondary" style={{float:'left'}} onClick={handleClose}>
+                    <Button variant="secondary" style={{ float: 'left' }} onClick={handleClose}>
                         Close
                     </Button>
                 </Modal.Footer>
             </Modal>
             <div className="App">
                 <h1>Account Info</h1>
-                {
-                    (userInfo) ? <>
-                        <p>Hello, {userInfo.username}</p>
-                        <p>your email: {userInfo.email}</p>
-                        <Row className="justify-content-center">
+                <br />
+                <Row className="justify-content-center">
+                    {
+                        (userInfo) ? <>
                             <Col md="auto">
                                 <Form.Group>
                                     <Form.Control type="title" value={calendarTitle} onChange={(e) => { setCalendarTitle(e.target.value) }} placeholder="New Calendar Title" />
                                 </Form.Group>
                             </Col>
                             <Col md="auto">
-                                <Button variant="info" onClick={(e) => addCalendar(e) }>Add Calendar</Button>{' '}
+                                <Button variant="info" onClick={(e) => addCalendar(e)}>Add Calendar</Button>{' '}
                             </Col>
-                        </Row>
-                    </> : ""
-                }
+                        </> : ""
+                    }
+                    <Col md="auto">
+                        <Form.Group>
+                            <Form.Control type="title" value={guestLink} onChange={(e) => { setGuestLink(e.target.value) }} placeholder="guest url" />
+                        </Form.Group>
+                    </Col>
+                    <Col md="auto">
+                        <Button variant="info" onClick={(e) => { }}>Import guest calendar</Button>{' '}
+                    </Col>
+                </Row>
             </div>
             <br />
             <Container>
                 {
                     (calendars) ?
-                        <Table>
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th> calendar name</th>
-                                    <th>actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {calendars.map((cal, id) => {
-                                    let c = cal.data.attributes
-                                    return (
-                                        <tr key={id}>
-                                            <td>{c.id}</td>
-                                            <td>{c.title}</td>
-                                            <td>
-                                                <Button variant="primary" onClick={() => { handleShow(c.id) }}>members</Button>{' '}
-                                                <Button variant="secondary" onClick={() => { view(c.id) }}>view</Button>{' '}
-                                                <Button variant="danger" onClick={() => { }}>delete</Button>{' '}
-                                            </td>
-                                        </tr>)
-                                })}
-                            </tbody>
-                        </Table>
+                        <>
+                            <h3 className="App">my calendar</h3>
+                            <Table>
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th> calendar name</th>
+                                        <th> type</th>
+                                        <th>actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {calendars.map((cal, id) => {
+                                        let c = cal.data.attributes
+                                        return (
+                                            <tr key={id}>
+                                                <td>{id + 1}</td>
+                                                <td>{c.title}</td>
+                                                <td>my calendar</td>
+                                                <td>
+                                                    <Button variant="primary" onClick={() => { handleShow(c.id) }}>members</Button>{' '}
+                                                    <Button variant="secondary" onClick={() => { view(c.id) }}>view</Button>{' '}
+                                                    {/* <Button variant="danger" onClick={() => { }}>delete</Button>{' '} */}
+                                                </td>
+                                            </tr>)
+                                    })}
+                                </tbody>
+                            </Table>
+                        </>
                         : "empty calendars"
                 }
             </Container>
